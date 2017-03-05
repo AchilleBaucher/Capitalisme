cbf2468a3df557f4ff2b062f39eb3f56b76d915c
acSpinoza <acSpinoza@users.noreply.github.com>
2017-03-05 11:49:01 +0100

    ?

--- cap.py
+++ cap.py
@@ -65,3 +65,4 @@ def etude(carte, coutMenuM,coutMenuQ, coutEntretien, pop, pref, prixM,prixQ ):
 def decision(carte, pop, pref, dicScoreM, dicScoreQ, dicProfitM, dicProfitQ):
 	"""Renvoie la decicsion mensuelle des sieges"""
 
+a= "jvcvcvlcvavvce"
