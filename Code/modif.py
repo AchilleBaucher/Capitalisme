2664facedcd79da2ebbf8c1dd8eee7bc523d9a37
acSpinoza <acSpinoza@users.noreply.github.com>
2017-03-06 21:38:21 +0100

    virgules enlevees

--- Code/modif.py
+++ Code/modif.py
@@ -1,4 +1,5 @@
 #C'est pour modifier les villes.
+
 #POUR AJOUTER DES VIRGULES:
 """
 fichier = open("villes.txt")
@@ -6,6 +7,43 @@ liste_lignes = fichier.readlines()
 new_liste = []
 for i in liste_lignes:
 	a = i[:-2] + ","
+	new_liste.append(a)
+fichier.close()
+print(new_liste[0])
+
+fichier = open("villes.txt","wcd")
+for i in new_liste:
+	fichier.write(i)
+	fichier.write("\n")
+fichier.close"""
+
+#POUR RETIRER DES VIRGULES
+
+fichier = open("villes.txt")
+liste_lignes = fichier.readlines()
+new_liste = []
+for i in liste_lignes:
+	a = i[:-3]
+	new_liste.append(a)
+fichier.close()
+print(new_liste[0])
+
+fichier = open("villes.txt","wcd")
+for i in new_liste:
+	fichier.write(i)
+	fichier.write("\n")
+fichier.close
+
+#Pour taper les attributs:
+"""
+fichier = open("villes.txt")
+liste_lignes = fichier.readlines()
+new_liste = []
+for i in liste_lignes:
+	liste_de_mots = i.strip().split(",")
+	ville = liste_de_mots[0]
+	b = raw_input((ville+":"))
+	a = i[:-2] + ',' + b
 	new_liste.append(a)
 fichier.close()
 print(new_liste[0])
