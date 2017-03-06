99fa73891a023de57419d60ddb66b6988ab493c9
acSpinoza <acSpinoza@users.noreply.github.com>
2017-03-06 21:26:38 +0100

    MODIF

--- Code/modif.py
+++ Code/modif.py
@@ -1,1 +1,17 @@
 #C'est pour modifier les villes.
+#POUR AJOUTER DES VIRGULES:
+"""
+fichier = open("villes.txt")
+liste_lignes = fichier.readlines()
+new_liste = []
+for i in liste_lignes:
+	a = i[:-2] + ","
+	new_liste.append(a)
+fichier.close()
+print(new_liste[0])
+
+fichier = open("villes.txt","wcd")
+for i in new_liste:
+	fichier.write(i)
+	fichier.write("\n")
+fichier.close"""
