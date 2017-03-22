4649841bdcd290a12d085f24ded3ba119a8d7353
gangdlolol <nicolas010797@laposte.net>
2017-03-22 12:33:32 +0100

    Update Map.py

--- Code/Map.py
+++ Code/Map.py
@@ -9,8 +9,10 @@ Pageprincipale.maxsize (width=728,height=589)
 Pageprincipale.minsize (width=728,height=589)
 Pageprincipale['bg']='Black'
 
+
+
 #photo
-PhotoIDF = PhotoImage(file="Carte Île de france.png")
+PhotoIDF = PhotoImage(file="CIDF.png")
 
 #Fond
 canvas = Canvas(Pageprincipale,width=728, height=589)
@@ -179,7 +181,7 @@ IssylesMoulineaux_coords = (272,217,285,225)
 IssylesMoulineaux = canvas.create_rectangle(*IssylesMoulineaux_coords)
 
 LaFertesousJouarre_coords = (565,150,575,160)
-LaFertésousJouarre= canvas.create_rectangle(*LaFertésousJouarre_coords)
+LaFertesousJouarre= canvas.create_rectangle(*LaFertesousJouarre_coords)
 
 Noisylegrand_coords = (370,205,380,220)
 Noisylegrand = canvas.create_rectangle(*Noisylegrand_coords)
@@ -242,43 +244,62 @@ Saintmaurdesfosses= canvas.create_rectangle(*Saintmaurdesfosses_coords)
 Nanterre_coords = (250,180,270,200)
 Nanterre= canvas.create_rectangle(*Nanterre_coords)
 
-
 canvas.create_image(0, 0, anchor=NW, image=PhotoIDF)
+
 
 
 coords = [Paris1_coords, Paris2_coords, Paris3_coords, Paris4_coords, Paris5_coords, Paris6_coords, Paris7_coords, Paris8_coords, Paris9_coords, Paris10_coords, Paris11_coords, Paris12_coords, Paris13_coords, Paris14_coords, Paris15_coords, Paris16_coords, Paris17_coords, Paris18_coords, Paris19_coords,Paris20_coords, StDenis_coords, BoulogneBilancourt_coords, Versailles_coords,Creteil_coords, NeuillySurSeine_coords, Cergy_coords, Rambouillet_coords, Meaux_coords, Evry_coords, Orsay_coords, Melun_coords, Etampes_coords, Goussainville_coords, poissy_coords, Villepinte_coords, Coulommiers_coords, ClayeSouilly_coords, BrieComteRobert_coords, Dourdan_coords, GretzArmainvilliers_coords, Thoiry_coords, LIsleAdam_coords, MagnyenVexin_coords, LesMureaux_coords, Limay_coords, MontereauFaultYonne_coords, Provins_coords, Nemours_coords, Guyancourt_coords, Elancourt_coords, Chessy_coords, Nangis_coords, IssylesMoulineaux_coords, LaFertesousJouarre_coords, Noisylegrand_coords, SaintGermainenLaye_coords, LaCellesaintCloud_coords, RosnysousBois_coords, Franconville_coords, Gonesse_coords, VillierssurMarne_coords, Torcy_coords, Plaisir_coords, Palaiseau_coords, ManteslaJolie_coords, Sarcelles_coords, LePerrayenYvelines_coords, DammartinenGoele_coords, Serris_coords, Bombon_coords, Bretignysurorge_coords, Avon_coords, Saintmaurdesfosses_coords, Nanterre_coords]
 
-Villes = [Paris1, Paris2, Paris3, Paris4, Paris5, Paris6, Paris7, Paris8, Paris9, Paris10, Paris11, Paris12, Paris13, Paris14, Paris15, Paris16, Paris17, Paris18, Paris19, Paris20, StDenis, BoulogneBilancourt, Versailles, Creteil, NeuillySurSeine, Cergy, Rambouillet, Meaux, Evry, Orsay, Melun, Etampes, Goussainville, Poissy, Villepinte, Coulommiers, ClayeSouilly, BrieComteRobert, Dourdan, GretzArmainvilliers, Thoiry, LIsleAdam, MagnyenVexin, LesMureaux, Limay, MontereauFaultYonne, Provins, Nemours, Guyancourt, Elancourt, Chessy, Nangis, IssylesMoulineaux, LaFertesousJouarre, Noisylegrand, SaintGermainenLaye, LaCellesaintCloud, RosnysousBois, Franconville, Gonesse, VillierssurMarne, Torcy, Plaisir, Palaiseau, ManteslaJolie, Sarcelles, LePerrayenYvelines, DammartinenGoele, Serris, Bombon, Bretignysurorge, Avon, Saintmaurdesfosses, Nanterre]
+Villes = [(Paris1, "Paris1"), (Paris2,"Paris2"), (Paris3,"Paris3"), (Paris4,"Paris4"), (Paris5,"Paris5"), (Paris6,"Paris6"), (Paris7,"Paris7"), (Paris8,"Paris8"), (Paris9,"Paris9"), (Paris10,"Paris10"), (Paris11,"Paris11"), (Paris12,"Paris12"),  (Paris13,"Paris13"), (Paris14,"Paris14"), (Paris15,"Paris15"), (Paris16,"Paris16"), (Paris17,"Paris17"), (Paris18,"Paris18"), (Paris19,"Paris19"), (Paris20,"Paris20"), (StDenis,"Saint-Denis"), (BoulogneBilancourt,"Boulogne-Billancourt"), (Versailles,"Versailles"), (Creteil,"Creteil"), (NeuillySurSeine,"Neuilly-Sur-Seine"), (Cergy,"Cergy"), (Rambouillet,"Rambouillet"),( Meaux,"Meaux"), (Evry,"Evry"), (Orsay,"Orsay"), (Melun,"Melun"), (Etampes,"Etampes"), (Goussainville,"Goussainville"), (Poissy,"Poissy"), (Villepinte,"Villepinte"), (Coulommiers,"Coulommiers"), (ClayeSouilly,"Claye-Souilly"), (BrieComteRobert,"Brie-Comte-Robert"), (Dourdan,"Dourdan"), (GretzArmainvilliers,"Gretz-Armainvilliers"), (Thoiry,"Thoiry"), (LIsleAdam,"L’Isle-Adam"), (MagnyenVexin,"Magny-en-Vexin"), (LesMureaux,"Les Mureaux"), (Limay,"Limay"), (MontereauFaultYonne,"Montereau-Fault-Yonne"), (Provins,"Provins"), (Nemours,"Nemours"), (Guyancourt,"Guyancourt"), (Elancourt,"Elancourt"), (Chessy,"Chessy"), (Nangis,"Nangis"), (IssylesMoulineaux,"Issy-les-Moulineux"), (LaFertesousJouarre,"La Ferté-sous-Jouarre"), (Noisylegrand,"Noisy-le-grand"), (SaintGermainenLaye,"Saint-Germain-en-Laye"), (LaCellesaintCloud,"La Celle-saint-Cloud"), (RosnysousBois,"Rosny-sous-Bois"), (Franconville,"Franconville"), (Gonesse,"Gonesse"), (VillierssurMarne,"Villiers-sur-Marne"), (Torcy,"Torcy"), (Plaisir,"Plaisir"), (Palaiseau,"Palaiseau"), (ManteslaJolie,"Mantes-la-Jolie"), (Sarcelles,"Sarcelles"), (LePerrayenYvelines,"Le Perray-en-Yvelines"), (DammartinenGoele,"Dammartin-en-Goele"), (Serris,"Serris"), (Bombon,"Bombon"), (Bretignysurorge,"Bretigny-sur-orge"), (Avon,"Avon"), (Saintmaurdesfosses,"Saint-maur-des-fosses"), (Nanterre,"Nanterre")]
 
 
 
-# création d'un point rouge pour un mc do et d'un point vert pour un quick
-DictMcDo = dict()
+# creation d'un point rouge pour un mc do et d'un point vert pour un quick
+DictMcDo = {'Paris15':0,'Saint-Denis': 5, 'Paris19': 1, 'Paris5': 1, 'Plaisir': 0, 'Paris4': 0, 'Paris1': 0, 'Etampes': 1, 'Paris18': 0, 'Thoiry': 0, 'Dourdan': 0, 'Claye-Souilly': 0, 'Magny-en-Vexin': 0, 'Gonesse': 0, 'Paris3': 0, 'Poissy': 0, 'Nangis': 0, 'Les Mureaux': 0, 'Melun': 0, 'Meaux': 0, 'Brie-Comte-Robert': 0, 'Paris13': 0, 'Paris17': 0, 'Paris16': 0, 'Paris20': 0, 'Villiers-sur-Marne': 0, 'Paris6': 0, 'Coulommiers': 0, 'Serris': 0, 'Limay': 0, 'Gretz-Armainvilliers': 0, 'Versailles': 0, 'La Celle-saint-Cloud': 0, 'Palaiseau': 0, 'Boulogne-Billancourt': 0, 'Villepinte': 0, 'Noisy-le-grand': 0, 'Chessy': 0, 'Saint-Germain-en-Laye': 0, 'Avon': 0, 'Nemours': 0, 'Bombon': 0, 'Franconville': 0, 'Paris10': 0, 'Goussainville': 0, 'Rambouillet': 0, 'Paris8': 0, 'Paris11': 0, 'Cergy': 0, 'Le Perray-en-Yvelines': 0, 'Guyancourt': 0, 'Paris7': 0, 'Montereau-Fault-Yonne': 0, 'Paris2': 0, 'La Ferté-sous-Jouarre': 0, 'Rosny-sous-Bois': 0, 'Provins': 0, 'Neuilly-Sur-Seine': 0, 'Paris14': 0, 'Dammartin-en-Goele': 0, 'Paris12': 0, 'Orsay': 0, 'Saint-maur-des-fosses': 0, 'Creteil': 0, 'Elancourt': 0, 'Torcy': 0, 'Evry': 0, 'Sarcelles': 0, 'Bretigny-sur-orge': 0, 'Mantes-la-Jolie': 0, 'Issy-les-Moulineux': 0, 'L’Isle-Adam': 0, 'Nanterre': 0, 'Paris9': 0}
 
-DictQuick = dict()
+
+DictQuick = {'Paris15':0,'Saint-Denis': 5, 'Paris19': 0, 'Paris5': 0, 'Plaisir': 0, 'Paris4': 0, 'Paris1': 0, 'Etampes': 0, 'Paris18': 0, 'Thoiry': 0, 'Dourdan': 0, 'Claye-Souilly': 0, 'Magny-en-Vexin': 0, 'Gonesse': 0, 'Paris3': 0, 'Poissy': 0, 'Nangis': 0, 'Les Mureaux': 0, 'Melun': 0, 'Meaux': 0, 'Brie-Comte-Robert': 0, 'Paris13': 0, 'Paris17': 0, 'Paris16': 0, 'Paris20': 0, 'Villiers-sur-Marne': 0, 'Paris6': 0, 'Coulommiers': 0, 'Serris': 0, 'Limay': 0, 'Gretz-Armainvilliers': 0, 'Versailles': 0, 'La Celle-saint-Cloud': 0, 'Palaiseau': 0, 'Boulogne-Billancourt': 0, 'Villepinte': 0, 'Noisy-le-grand': 0, 'Chessy': 0, 'Saint-Germain-en-Laye': 0, 'Avon': 0, 'Nemours': 0, 'Bombon': 0, 'Franconville': 0, 'Paris10': 0, 'Goussainville': 0, 'Rambouillet': 0, 'Paris8': 0, 'Paris11': 0, 'Cergy': 0, 'Le Perray-en-Yvelines': 0, 'Guyancourt': 0, 'Paris7': 0, 'Montereau-Fault-Yonne': 0, 'Paris2': 0, 'La Ferté-sous-Jouarre': 0, 'Rosny-sous-Bois': 0, 'Provins': 0, 'Neuilly-Sur-Seine': 0, 'Paris14': 0, 'Dammartin-en-Goele': 0, 'Paris12': 0, 'Orsay': 1, 'Saint-maur-des-fosses': 0, 'Creteil': 2, 'Elancourt': 0, 'Torcy': 0, 'Evry': 0, 'Sarcelles': 0, 'Bretigny-sur-orge': 3, 'Mantes-la-Jolie': 0, 'Issy-les-Moulineux': 0, 'L’Isle-Adam': 0, 'Nanterre': 0, 'Paris9': 0}
+
+SauvegardeDictionnaireMcDo = {'Paris15':0,'Saint-Denis': 0, 'Paris19': 0, 'Paris5': 0, 'Plaisir': 0, 'Paris4': 0, 'Paris1': 0, 'Etampes': 0, 'Paris18': 0, 'Thoiry': 0, 'Dourdan': 0, 'Claye-Souilly': 0, 'Magny-en-Vexin': 0, 'Gonesse': 0, 'Paris3': 0, 'Poissy': 0, 'Nangis': 0, 'Les Mureaux': 0, 'Melun': 0, 'Meaux': 0, 'Brie-Comte-Robert': 0, 'Paris13': 0, 'Paris17': 0, 'Paris16': 0, 'Paris20': 0, 'Villiers-sur-Marne': 0, 'Paris6': 0, 'Coulommiers': 0, 'Serris': 0, 'Limay': 0, 'Gretz-Armainvilliers': 0, 'Versailles': 0, 'La Celle-saint-Cloud': 0, 'Palaiseau': 0, 'Boulogne-Billancourt': 0, 'Villepinte': 0, 'Noisy-le-grand': 0, 'Chessy': 0, 'Saint-Germain-en-Laye': 0, 'Avon': 0, 'Nemours': 0, 'Bombon': 0, 'Franconville': 0, 'Paris10': 0, 'Goussainville': 0, 'Rambouillet': 0, 'Paris8': 0, 'Paris11': 0, 'Cergy': 0, 'Le Perray-en-Yvelines': 0, 'Guyancourt': 0, 'Paris7': 0, 'Montereau-Fault-Yonne': 0, 'Paris2': 0, 'La Ferté-sous-Jouarre': 0, 'Rosny-sous-Bois': 0, 'Provins': 0, 'Neuilly-Sur-Seine': 0, 'Paris14': 0, 'Dammartin-en-Goele': 0, 'Paris12': 0, 'Orsay': 0, 'Saint-maur-des-fosses': 0, 'Creteil': 0, 'Elancourt': 0, 'Torcy': 0, 'Evry': 0, 'Sarcelles': 0, 'Bretigny-sur-orge': 0, 'Mantes-la-Jolie': 0, 'Issy-les-Moulineux': 0, 'L’Isle-Adam': 0, 'Nanterre': 0, 'Paris9': 0}
+
+
+SauvegardeDictionnaireQuick = {'Paris15':0,'Saint-Denis': 0, 'Paris19': 0, 'Paris5': 0, 'Plaisir': 0, 'Paris4': 0, 'Paris1': 0, 'Etampes': 0, 'Paris18': 0, 'Thoiry': 0, 'Dourdan': 0, 'Claye-Souilly': 0, 'Magny-en-Vexin': 0, 'Gonesse': 0, 'Paris3': 0, 'Poissy': 0, 'Nangis': 0, 'Les Mureaux': 0, 'Melun': 0, 'Meaux': 0, 'Brie-Comte-Robert': 0, 'Paris13': 0, 'Paris17': 0, 'Paris16': 0, 'Paris20': 0, 'Villiers-sur-Marne': 0, 'Paris6': 0, 'Coulommiers': 0, 'Serris': 0, 'Limay': 0, 'Gretz-Armainvilliers': 0, 'Versailles': 0, 'La Celle-saint-Cloud': 0, 'Palaiseau': 0, 'Boulogne-Billancourt': 0, 'Villepinte': 0, 'Noisy-le-grand': 0, 'Chessy': 0, 'Saint-Germain-en-Laye': 0, 'Avon': 0, 'Nemours': 0, 'Bombon': 0, 'Franconville': 0, 'Paris10': 0, 'Goussainville': 0, 'Rambouillet': 0, 'Paris8': 0, 'Paris11': 0, 'Cergy': 0, 'Le Perray-en-Yvelines': 0, 'Guyancourt': 0, 'Paris7': 0, 'Montereau-Fault-Yonne': 0, 'Paris2': 0, 'La Ferté-sous-Jouarre': 0, 'Rosny-sous-Bois': 0, 'Provins': 0, 'Neuilly-Sur-Seine': 0, 'Paris14': 0, 'Dammartin-en-Goele': 0, 'Paris12': 0, 'Orsay': 0, 'Saint-maur-des-fosses': 0, 'Creteil': 0, 'Elancourt': 0, 'Torcy': 0, 'Evry': 0, 'Sarcelles': 0, 'Bretigny-sur-orge': 0, 'Mantes-la-Jolie': 0, 'Issy-les-Moulineux': 0, 'L’Isle-Adam': 0, 'Nanterre': 0, 'Paris9': 0}
 
 
 
 for i in range(0,len(Villes)):
     xhaut, yhaut, xbas, ybas = coords[i]
-    if  DictMcDo[i] != SauvegardeDictionnaireMcDo[i]:
-
-        for j in range(SauvegardeDictionnaireMcDo[i],DictMcDo[i]):
-
-            Emplacementx = random.randint(xhaut, xbas)
-            Emplacementy = random.randint(yhaut, ybas)
-            canvas.create_rectangle(Emplacementx, Emplacementy, Emplacementx, Emplacementy,fill = "red")
-
+    #for h  in DictMcDo:
+    nomVille = Villes[i][1]
+    h = DictMcDo[nomVille]
+    #a = SauvegardeDictionnaireMcDo[nomVille]
+    print(h)
+        #print(xhaut, yhaut, xbas, ybas, coords[i])    
+    #if a!=h: #DictMcDo[h] != SauvegardeDictionnaireMcDo[h]:
+       # for j in range(a,h):
+    if h!=0:
             
-     if  DictQuick[i] != SauvegardeDictionnaireQuick[i]:
+        Emplacementx = random.randint(xhaut, xbas)
+        Emplacementy = random.randint(yhaut, ybas)
+        print(Emplacementx, Emplacementy)
+        canvas.create_rectangle(Emplacementx, Emplacementy, Emplacementx+5, Emplacementy+5,fill = "red")
+        if h > 1:
+            canvas.create_text(Emplacementx, Emplacementy,text=h,fill = 'red')
+        
+    nomVille2 = Villes[i][1]
+    p = DictQuick[nomVille2]
+    #for k  in DictQuick:        
+    #if  DictQuick[k] != SauvegardeDictionnaireQuick[k]:
+    if p!= 0:
+    
+                #for j in range(SauvegardeDictionnaireQuick[k],DictQuick[k]):
+    
+        Emplacementx = random.randint(xhaut, xbas)
+        Emplacementy = random.randint(yhaut, ybas)
+        canvas.create_rectangle(Emplacementx, Emplacementy, Emplacementx+5, Emplacementy+5, fill = "green")    
 
-        for j in range(SauvegardeDictionnaireQuick[i],DictQuick[i]):
-
-            Emplacementx = random.randint(xhaut, xbas)
-            Emplacementy = random.randint(yhaut, ybas)
-            canvas.create_rectangle(Emplacementx, Emplacementy, Emplacementx, Emplacementy, fill = "green")        
-
-
+        if p > 1:
+            canvas.create_text(Emplacementx, Emplacementy,text=p,fill = 'green')
 
 
     
@@ -301,15 +322,3 @@ for i in range(0,len(Villes)):
 canvas.pack()
 
 Pageprincipale.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
